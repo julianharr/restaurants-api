abfae9a9d8e3c1023f4e8085b17d168643502528
Julian Harrington <jrayharrington@gmail.com>
2021-02-21 16:09:15 +1100

    added update method to restaurant controller

--- app/views/api/v1/restaurants/show.json.jbuilder
+++ app/views/api/v1/restaurants/show.json.jbuilder
@@ -1,4 +1,4 @@
 json.extract! @restaurant, :id, :name, :address
-json.comments @restaurant.comments do |comment|
+json.comments @restaurant.comments do |comment| # Loop over the comments and extract the ID and the content
   json.extract! comment, :id, :content
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Nest v1 within api so that your route looks like: api_vi_restaurants
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :restaurants, only: [ :index, :show ]
+      resources :restaurants, only: [ :index, :show, :update ]
     end
   end
 end
